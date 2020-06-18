f5913957006fc3f20ca80a4ad034f4ae8cd71331
Anthony Casper <tonycasper21@gmail.com>
2020-06-18 10:43:30 -0500

    sanitizes db query - quick fix

--- db/querys.js
+++ db/querys.js
@@ -17,7 +17,7 @@ connection.connect((err) => {
 //`SELECT cats.*, i.url, c.categoryName FROM cats, images i, categories c, cats_categories cc WHERE catName = '${catName}' AND cats.id=i.cat_id AND cats.id=cc.cat_id`
 connection.getCat = (catName) => {
   return new Promise ((resolve, reject) => {
-    connection.query(`SELECT * FROM cats INNER JOIN images ON (cats.catName='${catName}' AND cats.id=images.cat_id) INNER JOIN categories ON (categories.id = cats.category_id)`, (err, results) => {
+    connection.query(`SELECT * FROM cats INNER JOIN images ON (cats.catName=(?) AND cats.id=images.cat_id) INNER JOIN categories ON (categories.id = cats.category_id)`, catName, (err, results) => {
       if (err) {
         reject(err);
       } else {
