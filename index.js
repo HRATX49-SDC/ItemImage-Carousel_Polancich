721c263115cd6b13bc42945a161f230dadf91ade
Anthony Casper <tonycasper21@gmail.com>
2020-06-19 18:06:29 -0500

    heroku testing

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded( { extended: true } ));
 app.use(express.static(path.join(__dirname, 'client', 'dist')));
-
+//heroku
 app.get('/purrget', (req, res) => {
   db
     .getCat(req.query.catName)
