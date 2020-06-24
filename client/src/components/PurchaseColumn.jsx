f6dd7a05babc618a026d088127768b778fbbaac5
Anthony Casper <tonycasper21@gmail.com>
2020-06-24 14:33:42 -0500

    Implements CSS Modules for purchase column

--- client/src/components/PurchaseColumn.jsx
+++ client/src/components/PurchaseColumn.jsx
@@ -1,45 +1,46 @@
 import React from 'react';
+import styles from '../styles/purchase.css'
 
 export default function PurchaseColumn({ handlePurchase }) {
   return (
-    <div className="column-1-4" id="purchase-column">
-      <div className="fulfillment-boxes">
-        <div className="flex-row">
-          <div className="column-1-2 tiny-padding-right">
-            <span className="dark-green-bold">Pick Up Today</span>
+    <div className={styles.purchaseColumn}>
+      <div className={styles.fulfillmentBoxes}>
+        <div className={styles.flexRow}>
+          <div className={styles.halfColPaddingRight}>
+            <span className={styles.darkGreenBold}>Pick Up Today</span>
             <span> at </span>
-            <span className="mini-link dark-bold"> Austin Saltillo</span><br></br>
-            <div className="std-margin-bottom">
-              <a href="#" className="sm-text mini-link">Edit Store</a>
+            <span className={styles.darkLinkBold}> Austin Saltillo</span><br></br>
+            <div className={styles.stdMarginBot}>
+              <a href="#" className={styles.miniLink}>Edit Store</a>
             </div>
           </div>
-          <div className="column-1-2 tiny-padding-left">
-            <button className="red-button" onClick={handlePurchase}>Pick it up</button>
+          <div className={styles.halfColPaddingLeft}>
+            <button className={styles.redButton} onClick={handlePurchase}>Pick it up</button>
           </div>
-          <div className="md-text full-width">{'Ready in 4 hours for pickup inside the store. \n'}</div>
-          <div className="sm-text tiny-margin-top">Aisle 1</div>
+          <div className={styles.readyIn}>Ready in 4 hours for pickup inside the store.</div>
+          <div className={styles.aisle}>Aisle 1</div>
         </div>
       </div>
-      <div className="fulfillment-boxes" id="delivery-box">
-        <div className="flex-row">
-          <div className="column-1-2 tiny-padding-right">
-            <span className="dark-green-bold">Same Day Delivery</span>
+      <div className={styles.deliveryBox}>
+        <div className={styles.flexRow}>
+          <div className={styles.halfColPaddingRight}>
+            <span className={styles.darkGreenBold}>Same Day Delivery</span>
             <span> to </span>
-            <span className="dark-bold"> 78741</span><br></br>
-            <div className="std-margin-bottom">
-              <a href="#" className="sm-text mini-link">Edit Zip Code</a>
+            <span className={styles.darkBold}> 78741</span><br></br>
+            <div className={styles.stdMarginBot}>
+              <a href="#" className={styles.miniLink}>Edit Zip Code</a>
             </div>
           </div>
-          <div className="column-1-2 tiny-padding-left">
-            <button className="red-button" onClick={handlePurchase}>Deliver it</button>
+          <div className={styles.halfColPaddingLeft}>
+            <button className={styles.redButton} onClick={handlePurchase}>Deliver it</button>
           </div>
-          <div className="md-text">
-            <span className="dark-green-margin">Get it as soon as 9am tomorrow </span>
+          <div className={styles.mediumText}>
+            <span className={styles.darkGreenMargin}>Get it as soon as 9am tomorrow </span>
             <span>with Shipt</span>
-            <div className="dark-margin">Delivery windows are in high demand and can change quickly</div>
-            <div className="sm-text">Free with membership or $9.99/order</div>
+            <div className={styles.darkMargin}>Delivery windows are in high demand and can change quickly</div>
+            <div className={styles.smallText}>Free with membership or $9.99/order</div>
           </div>
-          <div className="sm-text tiny-margin-top mini-link">Learn more</div>
+          <div className={styles.learn}>Learn more</div>
         </div>
       </div>
     </div>
