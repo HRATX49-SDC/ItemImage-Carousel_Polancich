91a53821930150d2a5d1faeed3b03523a2d2eb16
Anthony Casper <tonycasper21@gmail.com>
2020-06-25 13:49:00 -0500

    Testing button replacement

--- client/src/components/PriceColumn.jsx
+++ client/src/components/PriceColumn.jsx
@@ -13,12 +13,27 @@ export default function PriceColumn({ price, currentQuantity, handleQuantityChan
         <div className={styles.quantityBox}>
           <div className={styles.quantitySelect}>
             <div className={styles.quantityTitle}>Quantity</div>
-            <select className={styles.quantityButton} value={currentQuantity} onChange={(e) => handleQuantityChange(e)}>
-              <option>1</option>
+            <button className={styles.quantityButton} value={currentQuantity} onChange={(e) => handleQuantityChange(e)}>
+              <div>1</div>
+              <svg width="24" height="24" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" focusable="false">
+                <defs>
+                  <path id="nds-Icon85a" d="M9.06 12.5l7.47-7.47-1.06-1.06-8.53 8.53 8.53 8.53 1.06-1.06z"></path>
+                </defs>
+                <use transform="scale(1 -1) rotate(90 24.235 0)"></use>
+              </svg>
+              {/* <option>1</option>
               <option>2</option>
               <option>3</option>
-              <option>4</option>
-            </select>
+              <option>4</option> */}
+            </button>
+            <button>
+            <svg width="24" height="24" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" focusable="false">
+                <defs>
+                  <path id="nds-Icon85a" d="M9.06 12.5l7.47-7.47-1.06-1.06-8.53 8.53 8.53 8.53 1.06-1.06z"></path>
+                </defs>
+                <use transform="scale(1 -1) rotate(90 24.235 0)"></use>
+            </svg>
+            </button>
           </div>
           {/* <i className="gg-chevron-down"></i> */}
         </div>
